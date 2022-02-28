1922cd1c8b9f8ed526b695c49758228582b9004b
Wesley Catuzzo <62621841+WesleyCatuzzo@users.noreply.github.com>
2022-02-28 13:42:41 -0300

    Tratamento do checklist

--- Semana-7/TRAINEE REACT/to-do-list/src/pages/Tasks/index.jsx
+++ Semana-7/TRAINEE REACT/to-do-list/src/pages/Tasks/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Page, Title, FieldInput, FieldInputCheck } from "./styles";
 import {AiFillPlusCircle} from 'react-icons/ai'
+import api from '../../services/api'
 
 function InputTask(){
     return (
@@ -11,12 +12,19 @@ function InputTask(){
     )
 }
 
-function InputCheck(){
+function InputCheck({name, done, id, onUpdate}){
+    const handleCheckTask = () => {
+        api.put(`/task/${id}`, {done: !done}).then(() => {
+            onUpdate()
+        }) .catch(error => {
+            console.log(error)
+        })
+    }
     return (
-        <FieldInputCheck htmlFor="check">
-            <input type= "checkbox" id="check" /> 
+        <FieldInputCheck htmlFor={name}>
+            <input type= "checkbox" id={name} defaultChecked={done} checked={done === true ? true: undefined} onChange={handleCheckTask}/> 
             <span />
-            sla   
+            {name}  
         </FieldInputCheck>
     )
 }
@@ -24,11 +32,36 @@ function InputCheck(){
 
 
 function Tasks() {
+    const [tasks, setTasks] = useState([])
+    const handleGetTasks = () => {
+        api.get('/task').then(response => {
+            setTasks(response.data)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+    useEffect(handleGetTasks,[])
+    const handleSubmit = async event => {
+        event.preventDefault()
+        const {value} = event.target.elements[0]
+        await api.post('/task', {name: value}).then(() =>{
+            handleGetTasks()
+        }).catch(error => {
+            console.log(error)
+        })
+    }
     return (
         <Page>
             <Title>TaskDo</Title>
-            <InputTask />
-            <InputCheck />
+            <form onSubmit={handleSubmit}>
+                <InputTask />
+            </form>
+            {
+                tasks.map(task => (
+
+                    <InputCheck {...task} onUpdate={handleGetTasks}/>
+                ))
+            }
         </Page>
     )
 
